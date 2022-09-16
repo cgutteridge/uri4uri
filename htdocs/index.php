46827867794eb17fdce0f1b297e7da3247025493
IllidanS4 <illidans4@gmail.com>
2022-09-16 14:48:40 +0200

    fix some edge case URI parsing

--- htdocs/index.php
+++ htdocs/index.php
@@ -58,18 +58,30 @@ function parse_url_fixed($uri)
   
   if(!$has_query && !$has_fragment)
   {
+    // Fix "a:0" treated as host+port
     $uri = "$uri?";
   }
   
   $result = parse_url($uri);
   
+  if(isset($result['host']) && isset($result['port']) && !isset($result['scheme']) && substr($uri, 0, 2) !== '//')
+  {
+    // Fix "a:0/" treated as host+port
+    $result['scheme'] = $result['host'];
+    unset($result['host']);
+    $result['path'] = "$result[port]$result[path]"; // 0 will be trimmed however
+    unset($result['port']);
+  }
+  
   if($has_query && !isset($result['query']))
   {
+    // Include empty but existing query
     $result['query'] = '';
   }
   
   if($has_fragment && !isset($result['fragment']))
   {
+    // Include empty but existing fragment
     $result['fragment'] = '';
   }
   
