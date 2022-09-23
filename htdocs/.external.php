c5f7747bec45ea51fb5dd8cf5184259c9f16bb7a
IllidanS4 <illidans4@gmail.com>
2022-09-23 02:18:51 +0200

    cache JSON data

--- htdocs/.external.php
+++ htdocs/.external.php
@@ -230,182 +230,128 @@ function update_iana_records($file, $assignments, $id_element, $combine_id)
   {
     file_put_contents($file, json_encode($records, JSON_UNESCAPED_SLASHES));
   }
+  
+  return $records;
+}
+
+function get_json_source($cache_file, $on_renew)
+{
+  static $in_memory = array();
+  if(isset($in_memory[$cache_file]))
+  {
+    return $in_memory[$cache_file];
+  }
+  
+  $data = get_updated_json_file($cache_file, $renew);
+  $in_memory[$cache_file] = $data;
+  if($renew)
+  {
+    ob_start();
+    register_shutdown_function(function($cache_file, $on_renew)
+    {
+      flush_output();
+      $on_renew($cache_file);
+    }, $cache_file, $on_renew);
+  }
+  
+  return $data;
 }
 
 function get_schemes()
 {
-  static $cache_file = __DIR__.'/data/schemes.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/schemes.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'uri-schemes', 'value', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'uri-schemes', 'value', false);
+  });
 }
 
 function get_mime_types()
 {
-  static $cache_file = __DIR__.'/data/mime.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/mime.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'media-types', 'name', true);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'media-types', 'name', true);
+  });
 }
 
 function get_special_domains()
 {
-  static $cache_file = __DIR__.'/data/specialdn.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/specialdn.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'special-use-domain-names', 'name', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'special-use-domain-names', 'name', false);
+  });
 }
 
 function get_urn_namespaces()
 {
-  static $cache_file = __DIR__.'/data/urnns.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/urnns.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'urn-namespaces', 'name', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'urn-namespaces', 'name', false);
+  });
 }
 
 function get_wellknown_uris()
 {
-  static $cache_file = __DIR__.'/data/wellknown.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/wellknown.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'well-known-uris', 'value', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'well-known-uris', 'value', false);
+  });
 }
 
 function get_ports()
 {
-  static $cache_file = __DIR__.'/data/ports.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/ports.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'service-names-port-numbers', 'number', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'service-names-port-numbers', 'number', false);
+  });
 }
 
 function get_protocols()
 {
-  static $cache_file = __DIR__.'/data/protocols.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/protocols.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
-    {
-      flush_output();
-      update_iana_records($cache_file, 'protocol-numbers', 'name', false);
-    }, $cache_file);
-  }
-  
-  return $data;
+    return update_iana_records($cache_file, 'protocol-numbers', 'name', false);
+  });
 }
 
 function get_tlds()
 {
-  static $cache_file = __DIR__.'/data/tld.json';
-  
-  $data = get_updated_json_file($cache_file, $renew);
-  if($renew)
+  return get_json_source(__DIR__.'/data/tld.json', function($cache_file)
   {
-    ob_start();
-    register_shutdown_function(function($cache_file)
+    libxml_set_streams_context(get_stream_context());
+    $html = new DOMDocument;
+    if(@$html->loadHTMLFile('https://www.iana.org/domains/root/db.html') === false)
     {
-      flush_output();
-      libxml_set_streams_context(get_stream_context());
-      $html = new DOMDocument;
-      if(@$html->loadHTMLFile('https://www.iana.org/domains/root/db.html') === false)
+      return;
+    }
+    $xpath = new DOMXPath($html);
+    
+    $domains = array();
+    foreach($xpath->query('//table[@id="tld-table"]/tbody/tr') as $domain_item)
+    {
+      $cells = iterator_to_array($xpath->query('td', $domain_item));
+      if(count($cells) === 3)
       {
-        return;
-      }
-      $xpath = new DOMXPath($html);
-      
-      $domains = array();
-      foreach($xpath->query('//table[@id="tld-table"]/tbody/tr') as $domain_item)
-      {
-        $cells = iterator_to_array($xpath->query('td', $domain_item));
-        if(count($cells) === 3)
+        foreach($xpath->query('.//a', $cells[0]) as $link)
         {
-          foreach($xpath->query('.//a', $cells[0]) as $link)
-          {
-            $domain_data = array();
-            $name = trim($link->textContent);
-            $domain_data['description'] = $name;
-            $id = ltrim($name, ".");
-            $domain_data['id'] = $id;
-            $domain_data['url'] = $link->getAttribute('href');
-            $domain_data['type'] = trim($cells[1]->textContent);
-            $domain_data['sponsor'] = trim($cells[2]->textContent);
-            $domains[$id] = $domain_data;
-            break;
-          }
+          $domain_data = array();
+          $name = trim($link->textContent);
+          $domain_data['description'] = $name;
+          $id = ltrim($name, ".");
+          $domain_data['id'] = $id;
+          $domain_data['url'] = $link->getAttribute('href');
+          $domain_data['type'] = trim($cells[1]->textContent);
+          $domain_data['sponsor'] = trim($cells[2]->textContent);
+          $domains[$id] = $domain_data;
+          break;
         }
       }
-      ksort($domains);
-      
-      if(file_exists($cache_file))
-      {
-        file_put_contents($cache_file, json_encode($domains, JSON_UNESCAPED_SLASHES));
-      }
-    }, $cache_file);
-  }
-  
-  return $data;
+    }
+    ksort($domains);
+    
+    if(file_exists($cache_file))
+    {
+      file_put_contents($cache_file, json_encode($domains, JSON_UNESCAPED_SLASHES));
+    }
+    return $domains;
+  });
 }
